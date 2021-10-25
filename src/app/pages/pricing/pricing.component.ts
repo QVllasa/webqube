185efa1ead0e50d5d50f39e704d400ec7b481ff0
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2021-10-25 18:04:45 +0200

    working on pricing page

--- src/app/pages/pricing/pricing.component.ts
+++ src/app/pages/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
@@ -7,14 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingComponent implements OnInit {
 
+  columnsKeys = ['Feature','Starter','Professional','Business']
+
+  featureComparison = [
+    {
+      name: 'tax savings',
+      included: ['starter', 'professional', 'business']
+    },
+    {
+      name: 'Easy to use accounting',
+      included: ['Starter','Professional','Business']
+    },
+    {
+      name: 'Multi-accounts',
+      included: ['Starter','Professional','Business']
+    },
+    {
+      name: 'Invoicing',
+      included: ['Starter','Professional','Business']
+    },
+    {
+      name: 'Mobile and web access',
+      included: ['Starter','Professional','Business']
+    }
+
+  ]
+
   isMonthly: boolean = true;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  switchPlan(){
+  switchPlan() {
     this.isMonthly = !this.isMonthly;
   }
 
