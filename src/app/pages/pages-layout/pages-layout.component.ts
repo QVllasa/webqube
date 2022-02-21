51ef2491d352cdd69623e5e5ac103fa8b0234723
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-02-21 23:49:21 +0100

    route to dashboard added after login

--- src/app/pages/pages-layout/pages-layout.component.ts
+++ src/app/pages/pages-layout/pages-layout.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RegisterComponent} from "../../../@webqube/components/dialogs/register/register.component";
 import {AngularFireFunctions} from "@angular/fire/compat/functions";
 import {MatDialog} from "@angular/material/dialog";
 import {AuthComponent} from "../../../@webqube/components/dialogs/auth/auth.component";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {take} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-layout',
@@ -12,17 +14,18 @@ import {AngularFireAuth} from "@angular/fire/compat/auth";
 })
 export class PagesLayoutComponent implements OnInit {
 
-  constructor(public dialog: MatDialog, public auth: AngularFireAuth) { }
+  constructor(public dialog: MatDialog, public auth: AngularFireAuth, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
   navigationItems = [
-    {path: '/', label:''},
-    {path:'our-work', label: 'Werke', badge:''},
-    {path:'tech-blog', label: 'Tech-Blog', badge:'coming soon'},
-    {path:'pricing', label: 'Preise', badge:''},
-    {path:'about-us', label: 'Über Uns', badge:''}
+    {path: '/', label: ''},
+    {path: 'our-work', label: 'Werke', badge: ''},
+    {path: 'tech-blog', label: 'Tech-Blog', badge: 'coming soon'},
+    {path: 'pricing', label: 'Preise', badge: ''},
+    {path: 'about-us', label: 'Über Uns', badge: ''}
   ]
 
   openDialog(): void {
@@ -36,8 +39,10 @@ export class PagesLayoutComponent implements OnInit {
   onLogin() {
     const dialogRef = this.dialog.open(AuthComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
+    dialogRef.afterClosed().pipe(take(1)).toPromise()
+      .then(()=>{
+        this.router.navigate(['/dashboard'])
+      })
+
   }
 }
