df593741b1ba92f96ba1f3cb9e96d59a8c30eaf3
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-02-24 18:09:59 +0100

    wip pricing page

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,9 +5,6 @@ import * as admin from "firebase-admin";
 const fb = admin.initializeApp();
 
 export const getEmailVerificationLink = functions.https.onCall((data: string, context) => {
-  console.log("inside function: ", data)
-  const email: string = data;
-  console.log("email: ", email)
-  return fb.auth().generateEmailVerificationLink(email);
+  return fb.auth().generateEmailVerificationLink(data);
 })
 
