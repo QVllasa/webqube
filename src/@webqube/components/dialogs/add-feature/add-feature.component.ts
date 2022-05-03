e4930cb0fdaf616cc2a6488d068a0bbcdd23b2e8
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2022-05-04 00:46:55 +0200

    wip addons

--- src/@webqube/components/dialogs/add-feature/add-feature.component.ts
+++ src/@webqube/components/dialogs/add-feature/add-feature.component.ts
@@ -2,6 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {environment} from "../../../../environments/environment";
 import {ICreateOrderRequest, IPayPalConfig} from "ngx-paypal";
+import {IFeatureDetail} from "../../../models/models";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {BoardService} from "../../../services/board.service";
 
 @Component({
   selector: 'app-add-feature',
@@ -11,71 +14,80 @@ import {ICreateOrderRequest, IPayPalConfig} from "ngx-paypal";
 export class AddFeatureComponent implements OnInit {
 
   onSuccess: boolean = false;
+  quantity: number = 1;
   public payPalConfig ?: IPayPalConfig;
-  constructor(public dialogRef: MatDialogRef<AddFeatureComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  constructor(public dialogRef: MatDialogRef<AddFeatureComponent>,
+              private afs: AngularFirestore,
+              private boardService: BoardService,
+              @Inject(MAT_DIALOG_DATA) public data: { key: string, value: IFeatureDetail }) {
+  }
 
   ngOnInit(): void {
-    console.log("add feature",this.data);
+    console.log("add feature", this.data);
+    this.initConfig();
   }
 
   private initConfig(): void {
-    // this.payPalConfig = {
-    //   currency: 'EUR',
-    //   clientId: environment.paypal.clientID,
-    //   createOrderOnClient: (data) => <ICreateOrderRequest>{
-    //     intent: 'CAPTURE',
-    //     purchase_units: [{
-    //       amount: {
-    //         currency_code: 'EUR',
-    //         value: this.value,
-    //         breakdown: {
-    //           item_total: {
-    //             currency_code: 'EUR',
-    //             value: this.value
-    //           }
-    //         }
-    //       },
-    //       items: [{
-    //         name: 'x ' + this.plan.label + ' | ' + this.board.label,
-    //         quantity: '1',
-    //         category: 'DIGITAL_GOODS',
-    //         unit_amount: {
-    //           currency_code: 'EUR',
-    //           value: this.value,
-    //         },
-    //       }]
-    //     }]
-    //   },
-    //   advanced: {
-    //     commit: 'true'
-    //   },
-    //   style: {
-    //     label: 'paypal',
-    //     layout: 'vertical',
-    //   },
-    //   onApprove: (data, actions) => {
-    //     actions.order.get().then((res: any) => {
-    //       return this.afs.collection('orders').add(res).then(() => {
-    //         this.board.paid = true;
-    //         return this.boardService.updateBoard(this.board).then(() => {
-    //           this.onSuccess = true;
-    //         })
-    //       })
-    //     });
-    //   },
-    //   onClientAuthorization: (data) => {
-    //     console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
-    //   },
-    //   onCancel: (data, actions) => {
-    //     console.log('OnCancel', data, actions);
-    //   },
-    //   onError: err => {
-    //     console.log('OnError', err);
-    //   },
-    //   onClick: (data, actions) => {
-    //     console.log('onClick', data, actions);
-    //   }
-    // };
+    this.payPalConfig = {
+      currency: 'EUR',
+      clientId: environment.paypal.clientID,
+      createOrderOnClient: (data) => <ICreateOrderRequest>{
+        intent: 'CAPTURE',
+        purchase_units: [{
+          amount: {
+            currency_code: 'EUR',
+            value: this.data.value.price + '',
+            breakdown: {
+              item_total: {
+                currency_code: 'EUR',
+                value: this.data.value.price + ''
+              }
+            }
+          },
+          items: [{
+            name: 'x ' + this.data.value.title,
+            quantity: this.quantity+'',
+            category: 'DIGITAL_GOODS',
+            unit_amount: {
+              currency_code: 'EUR',
+              value: this.data.value.price + '',
+            },
+          }]
+        }]
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'pay',
+        size: 'large',
+        layout: 'horizontal',
+      },
+      onApprove: (data, actions) => {
+        actions.order.get().then((res: any) => {
+          return this.afs.collection('orders').add(res).then(() => {
+            this.onSuccess = true;
+            // this.board.paid = true;
+            // return this.boardService.updateBoard(this.board).then(() => {
+            //   this.onSuccess = true;
+            // })
+          })
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      }
+    };
 
   }
 
